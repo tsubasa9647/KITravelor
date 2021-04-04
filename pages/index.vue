3ad3127aaf64fbca9f424b117d453614f3730cec
Tsubasa Imamura <imamura@mk.ces.kyutech.ac.jp>
2021-04-04 09:57:29 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>test-blog</h1>
+    <div v-for="(item,key) in items" :key="key">
+       <h2>{{ item.title }}</h2>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  async asyncData() {
+    const { data } = await axios.get(
+      "https://kit.microcms.io/api/v1/blogs",
+      {
+        headers: { "X-API-KEY": process.env.API_KEY }
+      }
+    );
+    return {
+      items: data.contents
+    };
+  }
+}
+</script>
+
+<style>
+ h1 {
+    display: block;
+    font-size: 3em;
+    text-align: center;
+    font-weight: bold;
+    border-top: solid 3px #364e96;
+    border-bottom: solid 3px #364e96;
+  }
+  h2 {
+    display: block;
+    font-size: 1.6em;
+    text-align: center;
+    font-weight: bold;
+    border: solid #ddd;
+    border-width: 0 0 1px 0;
+  }
+</style>
